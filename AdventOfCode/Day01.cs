bcc61357f333e5725b25e16246703628c8d7cbb7
Lyud Popov <lyud.popov@gmail.com>
2022-12-01 17:56:10 +0000

    Day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -4,14 +4,26 @@ namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly IEnumerable<string[]> _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllText(InputFilePath)
+            .Split(Environment.NewLine+Environment.NewLine).Select(x => x.Split(Environment.NewLine));
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        var max = _input.Max(x => x.Sum(Convert.ToInt32));
+        return new($"{max}");
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        var topThree = _input.Select(x => x.Sum(Convert.ToInt32))
+            .OrderByDescending(x => x)
+            .Take(3);
+
+        return new($"{topThree.Sum()}");
+    }
 }
