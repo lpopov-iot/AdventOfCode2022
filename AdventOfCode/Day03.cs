20b5beb5f636709e7d44f580528c0333d45f2290
Lyud Popov <lyud.popov@gmail.com>
2022-12-03 10:56:29 +0000

    Improvement

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -19,13 +19,12 @@ namespace AdventOfCode
 
             foreach (var backpack in _input)
             {
-                var compartment1 = backpack.Substring(0, backpack.Length / 2);
-                var compartment2 = backpack.Substring(backpack.Length / 2, backpack.Length - backpack.Length / 2);
+                var compartment1 = backpack[..(backpack.Length / 2)];
+                var compartment2 = backpack[(backpack.Length / 2)..];
 
                 var dupe = compartment1.Intersect(compartment2).First();
 
-                sum += ConvertCharItemToIntegerScore(dupe);
-            }
+                sum += dupe - (dupe > 96 ? 96 : 38);            }
 
             return new($"{sum}");
         }
@@ -43,30 +42,10 @@ namespace AdventOfCode
             foreach (var chunk in chunks)
             {
                 var dupe = chunk[0].Intersect(chunk[1]).Intersect(chunk[2]).First();
-                sum += ConvertCharItemToIntegerScore(dupe);
+                sum += dupe - (dupe > 96 ? 96 : 38);
             }
 
             return new($"{sum}");
-        }
-
-        private int ConvertCharItemToIntegerScore(char c)
-        {
-            string itemTypes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-            int output = 0;
-            int ascii = c;
-            if (ascii >= 97 && ascii <= 122)
-            {
-                // Convert lowercase characters to the range 1-26
-                output = ascii - 96;
-            }
-            else if (ascii >= 65 && ascii <= 90)
-            {
-                // Convert uppercase characters to the range 27-52
-                output = ascii - 38;
-            }
-
-            return output;
         }
     }
 }
